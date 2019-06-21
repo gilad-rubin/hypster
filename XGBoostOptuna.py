78a41714146b2d0bb9fc466cc7e03db22e625d61
gilad-rubin <gilad.rubin@gmail.com>
2019-06-22 01:45:26 +0300

    XGBoostClassifier working with LR

--- XGBoostOptuna.py
+++ XGBoostOptuna.py
@@ -1,19 +1,37 @@
-from sklearn.base import ClassifierMixin
-# %%
-from sklearn.base import clone
-from copy import deepcopy
-from sklearn.utils import safe_indexing
-
+import xgboost as xgb
+from xgboost import XGBClassifier
+import numpy as np
 
 class XGBClassifierOptuna(object):
-    def __init__(self, lr_decay=0.5, seed=42):
+    def __init__(self, lr_decay=0.5, booster_list=['gblinear', 'gbtree', 'dart'],
+                 seed=None, user_param_dict={}):
+
+        #TODO: add "verbose" and other parameters (user_param_dict)
+        #TODO: add support for argument "missing"
+
         self.random_state = seed
         self.lr_decay = lr_decay
+        self.booster_list = booster_list
+        self.user_param_dict = user_param_dict
         self.n_estimators = 0
         self.learning_rates = []
 
+    def get_properties(self):
+        return {'shortname': 'XGBoost Classifier',
+                'name': 'XGBoost Classifier',
+                'handles_regression': False,
+                'handles_classification': True,
+                'handles_multiclass': True,
+                'handles_multilabel': False,
+                'is_deterministic': False,
+                # Both input and output must be tuple(iterable)
+                #'input': [DENSE, SIGNED_DATA, UNSIGNED_DATA],
+                #'output': [PREDICTIONS]
+                }
+
     def set_train(self, X_train, y_train):
-        self.dtrain = xgb.DMatrix(X_train, label=np.array(y_train))
+        #TODO: use XGBLabelEncoder
+        self.dtrain = xgb.DMatrix(X_train, label=np.array(y_train), )
 
     def set_test(self, X_test, y_test):
         self.dtest = xgb.DMatrix(X_test, label=np.array(y_test))
@@ -26,7 +44,7 @@ class XGBClassifierOptuna(object):
             , 'scale_pos_weight': pos_weight
             , 'objective': 'binary:logistic'
             , 'eta': trial.suggest_loguniform('init_eta', 1e-3, 1.0)
-            , 'booster': trial.suggest_categorical('booster', ['gblinear'])  # , 'gbtree', 'dart'])
+            , 'booster': trial.suggest_categorical('booster', self.booster_list)
             , 'lambda': trial.suggest_loguniform('lambda', 1e-10, 1.0)
             , 'alpha': trial.suggest_loguniform('alpha', 1e-10, 1.0)
                         }
@@ -40,7 +58,7 @@ class XGBClassifierOptuna(object):
                 , 'colsample_bytree': trial.suggest_uniform('colsample_bytree', 0.1, 1.0)
                 , 'colsample_bynode': trial.suggest_uniform('colsample_bynode', 0.1, 1.0)
                 , 'num_parallel_tree': trial.suggest_int('num_parallel_tree', 1, 20)
-                         }
+                }
 
             model_params.update(tree_dict)
 
@@ -53,15 +71,21 @@ class XGBClassifierOptuna(object):
                 , 'normalize_type': trial.suggest_categorical('normalize_type', ['tree', 'forest'])
                 , 'rate_drop': trial.suggest_loguniform('rate_drop', 1e-8, 1.0)
                 , 'skip_drop': trial.suggest_loguniform('skip_drop', 1e-8, 1.0)
-                         }
+                }
 
             model_params.update(dart_dict)
+
+        #TODO: add the option to run over default HP
+        # if self.user_param_dict is not None:
+        #     for (key, value) in self.user_param_dict:
+        #         self.model_params[key] = self.user_param_dict[key]
 
         self.model_params = model_params
 
     def train_one_iteration(self):
         if self.n_estimators == 0:
             init_model = None
+            self.init_eta = self.model_params["eta"]
         else:
             init_model = self.best_model
             init_model.set_param({"eta": self.model_params['eta']})
@@ -79,6 +103,7 @@ class XGBClassifierOptuna(object):
 
     def lower_complexity(self):
         self.model_params['eta'] = self.model_params['eta'] * self.lr_decay
+        return False
 
     def save_best(self):
         self.best_model = self.model.copy()
@@ -86,32 +111,56 @@ class XGBClassifierOptuna(object):
         self.n_estimators += 1
 
     def create_model(self):
-        final_model = XGBClassifierLR(self.model_params, self.n_estimators, self.learning_rates)
+        self.model_params['n_estimators'] = self.n_estimators
+        self.model_params['learning_rate'] = self.init_eta
+
+        self.model_params['n_jobs'] = self.model_params.pop('nthread')
+        self.model_params['random_state'] = self.model_params.pop('seed')
+        self.model_params['reg_lambda'] = self.model_params.pop('lambda')
+        self.model_params['reg_alpha'] = self.model_params.pop('alpha')
+
+        final_model = XGBClassifierLR(learning_rates=self.learning_rates, **self.model_params)
+
         return final_model
 
-# %%
-class XGBClassifierLR(ClassifierMixin):
-    def __init__(self, model_params=None, n_estimators=None, learning_rates=None):
-        self.learning_rates = learning_rates
-        self.model_params = model_params
-        self.n_estimators = n_estimators
+class XGBClassifierLR(XGBClassifier):
+    def __init__(self, learning_rates = None,
+                 max_depth=3, learning_rate=0.1, n_estimators=100,
+                 verbosity=1, silent=True,
+                 objective="binary:logistic", booster='gbtree',
+                 n_jobs=1, nthread=None, gamma=0, min_child_weight=1, max_delta_step=0,
+                 subsample=1, colsample_bytree=1, colsample_bylevel=1,
+                 colsample_bynode=1, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
+                 base_score=0.5, random_state=0, seed=None, missing=None, **kwargs):
 
-    def fit(self, X, y, sample_weight=None):
-        dtrain = xgb.DMatrix(X, label=y)
-        model = xgb.train(self.model_params
-                          , dtrain
-                          , num_boost_round=self.n_estimators
-                          , learning_rates=self.learning_rates
-                          )
-        self.model = model
+        if 'learning_rates' in kwargs:
+            self.learning_rates = kwargs.pop('learning_rates')
+        else:
+            self.learning_rates = learning_rates
 
-    def predict(self, X):
-        test_dmatrix = xgb.DMatrix(X)
-        return self.model.predict(test_dmatrix, output_margin=False)
+        super(XGBClassifierLR, self).__init__(
+            max_depth=max_depth, learning_rate=learning_rate, n_estimators=n_estimators,
+            verbosity=verbosity, silent=silent, objective=objective, booster=booster,
+            n_jobs=n_jobs, nthread=nthread, gamma=gamma,
+            min_child_weight=min_child_weight, max_delta_step=max_delta_step,
+            subsample=subsample, colsample_bytree=colsample_bytree,
+            colsample_bylevel=colsample_bylevel, colsample_bynode=colsample_bynode,
+            reg_alpha=reg_alpha, reg_lambda=reg_lambda, scale_pos_weight=scale_pos_weight,
+            base_score=base_score, random_state=random_state, seed=seed, missing=missing,
+            **kwargs)
 
-    def predict_proba(self, X):
-        test_dmatrix = xgb.DMatrix(X)
-        return self.model.predict(test_dmatrix, output_margin=True)
 
-    def get_params(self):
-        return self.learning_rates
+    def fit(self, X, y, sample_weight=None, eval_set=None, eval_metric=None,
+            early_stopping_rounds=None, verbose=True, xgb_model=None,
+            sample_weight_eval_set=None, callbacks=None):
+
+        lr_callback = [xgb.callback.reset_learning_rate(self.learning_rates)]
+
+        if callbacks is not None:
+            callbacks = callbacks + lr_callback
+        else:
+            callbacks = lr_callback
+
+        #TODO: check if there's already a "reset_learning_rate" callback and decide what to do
+
+        return super(XGBClassifierLR, self).fit(X, y, callbacks = callbacks)
--- optuna_utils.py
+++ optuna_utils.py
@@ -2,35 +2,30 @@ import pandas as pd
 import numpy as np
 import scipy
 import sklearn
+import optuna
+
+from sklearn.base import clone
+from copy import deepcopy
+from sklearn.utils import safe_indexing
+from sklearn.pipeline import Pipeline
 
 class Objective(object):
-    def __init__(self
-                 , X
-                 , y
-                 , pipeline
-                 , pipe_params
-                 , estimator
-                 , tol=1e-7
-                 , cv=5
-                 , groups=None
-                 , agg_func=np.mean
-                 , max_iter=1000
-                 , max_fails=10
-                 , scoring=sklearn.metrics.balanced_accuracy_score
-                 # ,num_iter_check=5
-                 ):
+    def __init__(self, X, y, estimator, pipeline=None, pipe_params=None,
+                 cv='warn', scoring=None,  # sklearn, support multiple?
+                 refit=False, agg_func=np.mean, tol=1e-5, max_iter=5000, max_fails=3):
+
         self.X = X
         self.y = y
         self.pipeline = pipeline
         self.pipe_params = pipe_params
         self.estimator = estimator
-        self.tol = tol
         self.cv = cv
+        self.scoring = scoring
+        self.refit = refit
         self.agg_func = agg_func
-        self.groups = groups
+        self.tol = tol
         self.max_iter = max_iter
         self.max_fails = max_fails
-        self.scoring = scoring
 
     def __call__(self, trial):
         if self.pipeline is not None:
@@ -42,17 +37,15 @@ class Objective(object):
         estimator_list = []
 
         # create k folds and estimators
-        for train, test in self.cv.split(self.X, self.y, groups=self.groups):
+        for train, test in self.cv.split(self.X, self.y):  # groups=self.groups
             X_train, y_train = safe_indexing(self.X, train), safe_indexing(self.y, train)
             X_test, y_test = safe_indexing(self.X, test), safe_indexing(self.y, test)
-
-            if pipeline is not None:
+            if self.pipeline is not None:
                 X_train = pipeline.fit_transform(X_train, y_train)
                 X_test = pipeline.transform(X_test)
 
             self.estimator.choose_and_set_params(trial, weights)
-            estimator = deepcopy(self.estimator)
-
+            estimator = deepcopy(self.estimator) #TODO: check if "copy" or "clone" is better
             estimator.set_train(X_train, y_train)
             estimator.set_test(X_test, y_test)
 
@@ -60,6 +53,7 @@ class Objective(object):
 
         best_score = 0.0
         for step in range(self.max_iter):
+            # print("Iteration #", step)
             scores = []
             for estimator in estimator_list:
                 estimator.train_one_iteration()
@@ -69,7 +63,7 @@ class Objective(object):
             intermediate_value = self.agg_func(scores)
             trial.report(intermediate_value, step)
 
-            # print("intermediate result = ", intermediate_value)
+            #print("intermediate result = ", intermediate_value)
 
             if trial.should_prune(step):
                 raise optuna.structs.TrialPruned()
@@ -81,7 +75,7 @@ class Objective(object):
                     estimator.save_best()
             else:
                 fail_count += 1
-                if (fail_count >= self.max_fails) or (not hasattr(estimator_list[0], 'lower_complexity')):
+                if (fail_count >= self.max_fails) or (estimator_list[0].lower_complexity()==False):
                     break
 
                 # TODO: make this step only after k times
@@ -89,7 +83,7 @@ class Objective(object):
                     estimator.lower_complexity()
 
         model = estimator.create_model()
-        if pipeline is not None:
+        if self.pipeline is not None:
             pipeline.steps.append(['classifier', model])
         else:
             pipeline = Pipeline([("classifier", model)])
@@ -104,3 +98,78 @@ class Objective(object):
         param_dict = {name: trial._suggest(name, distribution) for name,
                                                                    distribution in self.pipe_params.items()}
         return param_dict
+
+
+class Study():
+    def __init__(self, X, y, estimators,  # names and/or instances,
+                 pipeline=None, pipe_params=None,
+                 cv='warn', scoring=None,  # sklearn, support multiple?
+                 greater_is_better=True,  # TODO: check how to extract from scorer/metric
+                 refit=False, agg_func=np.mean, tol=1e-5, max_iter=1000, max_fails=5,
+                 study_name="",  # TODO: think how to allow the user to define if they want to keep training or not?
+                 save_only_best_estimators=False,  # TODO: think about it
+                 verbosity=1, pruner=optuna.pruners.SuccessiveHalvingPruner(min_resource=2, reduction_factor=2),
+                 sampler=optuna.samplers.TPESampler(), random_state=None):
+
+        self.X = X
+        self.y = y
+        self.pipeline = pipeline
+        self.pipe_params = pipe_params
+        self.estimators = estimators
+        self.cv = cv
+        self.scoring = scoring
+        self.greater_is_better = greater_is_better
+        self.refit = refit
+        self.agg_func = agg_func
+        self.tol = tol
+        self.max_iter = max_iter
+        self.max_fails = max_fails
+        # self.study_name = study_name TODO:needed? maybe replace with "resume last study"?
+        self.save_only_best_estimators = save_only_best_estimators
+        self.verbosity = verbosity
+        self.pruner = pruner
+        self.sampler = sampler
+        self.random_state = random_state
+
+        # TODO: set_seed function for samplers & cv_function(?) with random_state
+
+    def run(self, n_trials_per_estimator=10, n_jobs=1):
+        studies = []
+        for i in range(len(self.estimators)):
+            estimator = self.estimators[i]
+
+            print(estimator.get_properties()["name"]) #TODO: convert to static method?
+
+            objective = Objective(self.X, self.y, estimator, self.pipeline, self.pipe_params,
+                                  cv=self.cv, scoring=self.scoring, agg_func=self.agg_func,
+                                  tol=self.tol, max_iter=self.max_iter, max_fails=self.max_fails)
+
+            if self.greater_is_better:
+                direction = "maximize"
+            else:
+                direction = "minimize"
+
+            if self.verbosity > 0:
+                optuna.logging.set_verbosity(optuna.logging.WARN)
+                # TODO: change
+
+            study = optuna.create_study(pruner=self.pruner, sampler=self.sampler, direction=direction)
+            studies.append(study)
+
+            if type(n_trials_per_estimator) == list:
+                n_trials = n_trials_per_estimator[i]
+            else:
+                n_trials = n_trials_per_estimator
+
+            studies[i].optimize(objective, n_trials=n_trials, n_jobs=n_jobs)
+
+        self.studies = studies
+        # append results
+        # find best result + refit
+        # output combined results
+        # return studies
+
+    def visualize_results(self):
+        return
+        # TODO: plot... with matplotlib/plotly/hvplot
+        # TODO: show if it will help to increase max_iter
