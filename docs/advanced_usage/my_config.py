1ee56e689695129974d811d08990e211e3114b3f
gilad-rubin <gilad.rubin@gmail.com>
2024-08-28 11:12:23 +0300

    added multiple sections to the docs

---
+++ docs/advanced_usage/my_config.py
@@ -0,0 +1,5 @@
+from hypster import HP
+
+def my_config(hp: HP):
+    llm_model = hp.select({'haiku': 'claude-3-haiku-20240307', 'sonnet': 'claude-3-5-sonnet-20240620', 'gpt-4o-mini': 'gpt-4o-mini'}, default='gpt-4o-mini')
+    llm_config = {'temperature': hp.number_input(0), 'max_tokens': hp.number_input(64)}
