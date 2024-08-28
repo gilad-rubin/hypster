672cfea938c1a40b206ff2118af00c641934a4ea
gilad-rubin <gilad.rubin@gmail.com>
2024-08-28 16:33:53 +0300

    removed old tests from the previous API

--- tests/test10.py
+++
@@ -1,8 +0,0 @@
-from classes import CacheManager, DiskCache, SqlCache
-from hypster import Options, lazy
-
-lazy([CacheManager, DiskCache, SqlCache], update_globals=True)
-
-disk_cache = DiskCache(path=Options(["/tmp", "/var/tmp"], default="/tmp"))
-sql_cache = SqlCache(table="cache")
-cache_manager = CacheManager(cache=Options([disk_cache, sql_cache]))
--- tests/test5.py
+++
@@ -1,7 +0,0 @@
-from classes import CacheManager, DiskCache
-from hypster import Options, lazy
-
-CacheManager = lazy(CacheManager)
-DiskCache = lazy(DiskCache)
-
-cache_manager = CacheManager(cache=DiskCache(path=Options(["/tmp", "/var/tmp"], default="/tmp")))
