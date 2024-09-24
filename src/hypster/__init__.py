f06d6878b196ef09fa386a5c1b452bd28f86644d
gilad-rubin <gilad.rubin@gmail.com>
2024-09-24 10:11:19 +0300

    added selection_handler to init

--- src/hypster/__init__.py
+++ src/hypster/__init__.py
@@ -2,6 +2,7 @@ from .config import config
 from .core import Hypster, load, save
 from .hp import HP
 from .logging_utils import configure_logging
+from .selection_handler import SelectionHandler
 from .utils import query_combinations
 
-__all__ = ["config", "save", "load", "HP"]
+__all__ = ["config", "save", "load", "HP", "SelectionHandler"]
