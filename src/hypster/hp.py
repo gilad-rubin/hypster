0450e94e6b840e31ff996c7364e64a755d347f19
gilad-rubin <gilad.rubin@gmail.com>
2024-10-28 22:26:47 +0200

    added min, max for numeric inputs

--- src/hypster/hp.py
+++ src/hypster/hp.py
@@ -62,41 +62,67 @@ class HP:
     def text_input(self, default: str, *, name: Optional[str] = None) -> str:
         text_input_call = TextInputCall(name=name, default=default)
         logger.debug(f"Added TextInputCall: {name}")
-        return text_input_call.execute(self.overrides)
+        return text_input_call.execute(self.selections, self.overrides)
 
     def multi_text(self, default: List[str] = [], *, name: Optional[str] = None) -> List[str]:
         multi_text_call = MultiTextCall(name=name, default=default)
         logger.debug(f"Added MultiTextCall: {name}")
-        return multi_text_call.execute(self.overrides)
+        return multi_text_call.execute(self.selections, self.overrides)
 
-    def number_input(self, default: Union[int, float], *, name: Optional[str] = None) -> Union[int, float]:
-        number_input_call = NumberInputCall(name=name, default=default)
+    def number_input(
+        self,
+        default: Union[int, float],
+        *,
+        name: Optional[str] = None,
+        min: Optional[Union[int, float]] = None,
+        max: Optional[Union[int, float]] = None,
+    ) -> Union[int, float]:
+        number_input_call = NumberInputCall(name=name, default=default, min=min, max=max)
         logger.debug(f"Added NumberInputCall: {name}")
-        return number_input_call.execute(self.overrides)
+        return number_input_call.execute(self.selections, self.overrides)
 
     def multi_number(
-        self, default: List[Union[int, float]] = [], *, name: Optional[str] = None
+        self,
+        default: List[Union[int, float]] = [],
+        *,
+        name: Optional[str] = None,
+        min: Optional[Union[int, float]] = None,
+        max: Optional[Union[int, float]] = None,
     ) -> List[Union[int, float]]:
-        multi_number_call = MultiNumberCall(name=name, default=default)
+        multi_number_call = MultiNumberCall(name=name, default=default, min=min, max=max)
         logger.debug(f"Added MultiNumberCall: {name}")
-        return multi_number_call.execute(self.overrides)
+        return multi_number_call.execute(self.selections, self.overrides)
 
-    def int_input(self, default: int, *, name: Optional[str] = None) -> int:
-        int_input_call = IntInputCall(name=name, default=default)
+    def int_input(
+        self,
+        default: int,
+        *,
+        name: Optional[str] = None,
+        min: Optional[int] = None,
+        max: Optional[int] = None,
+    ) -> int:
+        int_input_call = IntInputCall(name=name, default=default, min=min, max=max)
         logger.debug(f"Added IntInputCall: {name}")
-        return int_input_call.execute(self.overrides)
+        return int_input_call.execute(self.selections, self.overrides)
 
-    def multi_int(self, default: List[int] = [], *, name: Optional[str] = None) -> List[int]:
-        multi_int_call = MultiIntCall(name=name, default=default)
+    def multi_int(
+        self,
+        default: List[int] = [],
+        *,
+        name: Optional[str] = None,
+        min: Optional[int] = None,
+        max: Optional[int] = None,
+    ) -> List[int]:
+        multi_int_call = MultiIntCall(name=name, default=default, min=min, max=max)
         logger.debug(f"Added MultiIntCall: {name}")
-        return multi_int_call.execute(self.overrides)
+        return multi_int_call.execute(self.selections, self.overrides)
 
     def bool_input(self, default: bool, *, name: Optional[str] = None) -> bool:
         bool_input_call = BoolInputCall(name=name, default=default)
         logger.debug(f"Added BoolInputCall: {name}")
-        return bool_input_call.execute(self.overrides)
+        return bool_input_call.execute(self.selections, self.overrides)
 
     def multi_bool(self, default: List[bool] = [], *, name: Optional[str] = None) -> List[bool]:
         multi_bool_call = MultiBoolCall(name=name, default=default)
         logger.debug(f"Added MultiBoolCall: {name}")
-        return multi_bool_call.execute(self.overrides)
+        return multi_bool_call.execute(self.selections, self.overrides)
