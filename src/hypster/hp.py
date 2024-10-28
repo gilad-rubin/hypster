29dd390dd6c8d8d48189fce074337826c889b389
gilad-rubin <gilad.rubin@gmail.com>
2024-10-28 22:05:32 +0200

    added value calls

--- src/hypster/hp.py
+++ src/hypster/hp.py
@@ -1,19 +1,19 @@
 import logging
 from collections import OrderedDict
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 from .hp_calls import (
-    # BoolInputCall,
-    # IntInputCall,
-    # MultiBoolCall,
-    # MultiIntCall,
-    # MultiNumberCall,
+    BoolInputCall,
+    IntInputCall,
+    MultiBoolCall,
+    MultiIntCall,
+    MultiNumberCall,
     MultiSelectCall,
-    # MultiTextCall,
-    # NumberInputCall,
+    MultiTextCall,
+    NumberInputCall,
     OptionsType,
     SelectCall,
-    # TextInputCall,
+    TextInputCall,
     ValidKeyType,
 )
 
@@ -50,7 +50,7 @@ class HP:
         options: OptionsType,
         *,
         name: Optional[str] = None,
-        default: Optional[ValidKeyType] = None,
+        default: Optional[List[ValidKeyType]] = None,
         disable_overrides: bool = False,
     ) -> List[Any]:
         multi_select_call = MultiSelectCall(
@@ -59,48 +59,44 @@ class HP:
         logger.debug(f"Added MultiSelectCall: {name}")
         return multi_select_call.execute(self.selections, self.overrides)
 
-    # def text_input(self, default: str, *, name: Optional[str] = None) -> str:
-    #     text_input_call = TextInputCall(self, name=name, default=default)
-    #     logger.debug(f"Added TextInputCall: {name}")
-    #     return text_input_call.execute(self.selections, self.overrides)
+    def text_input(self, default: str, *, name: Optional[str] = None) -> str:
+        text_input_call = TextInputCall(name=name, default=default)
+        logger.debug(f"Added TextInputCall: {name}")
+        return text_input_call.execute(self.overrides)
 
-    # def number_input(self, default: Union[int, float], *, name: Optional[str] = None) -> Union[int, float]:
-    #     number_input_call = NumberInputCall(self, name=name, default=default)
-    #     logger.debug(f"Added NumberInputCall: {name}")
-    #     return number_input_call.execute(self.selections, self.overrides)
+    def multi_text(self, default: List[str] = [], *, name: Optional[str] = None) -> List[str]:
+        multi_text_call = MultiTextCall(name=name, default=default)
+        logger.debug(f"Added MultiTextCall: {name}")
+        return multi_text_call.execute(self.overrides)
 
-    # def int_input(self, default: int, *, name: Optional[str] = None) -> int:
-    #     int_input_call = IntInputCall(self, name=name, default=default)
-    #     logger.debug(f"Added IntInputCall: {name}")
-    #     return int_input_call.execute()
+    def number_input(self, default: Union[int, float], *, name: Optional[str] = None) -> Union[int, float]:
+        number_input_call = NumberInputCall(name=name, default=default)
+        logger.debug(f"Added NumberInputCall: {name}")
+        return number_input_call.execute(self.overrides)
 
-    # def multi_text(self, default: List[str] = [], *, name: Optional[str] = None) -> List[str]:
-    #     multi_text_call = MultiTextCall(self, name=name, default=default)
-    #     logger.debug(f"Added MultiTextCall: {name}")
-    #     return multi_text_call.execute()
+    def multi_number(
+        self, default: List[Union[int, float]] = [], *, name: Optional[str] = None
+    ) -> List[Union[int, float]]:
+        multi_number_call = MultiNumberCall(name=name, default=default)
+        logger.debug(f"Added MultiNumberCall: {name}")
+        return multi_number_call.execute(self.overrides)
 
-    # def multi_number(
-    #     self, default: List[Union[int, float]] = [], *, name: Optional[str] = None
-    # ) -> List[Union[int, float]]:
-    #     multi_number_call = MultiNumberCall(self, name=name, default=default)
-    #     logger.debug(f"Added MultiNumberCall: {name}")
-    #     return multi_number_call.execute()
+    def int_input(self, default: int, *, name: Optional[str] = None) -> int:
+        int_input_call = IntInputCall(name=name, default=default)
+        logger.debug(f"Added IntInputCall: {name}")
+        return int_input_call.execute(self.overrides)
 
-    # def multi_int(self, default: List[int] = [], *, name: Optional[str] = None) -> List[int]:
-    #     multi_int_call = MultiIntCall(self, name=name, default=default)
-    #     logger.debug(f"Added MultiIntCall: {name}")
-    #     return multi_int_call.execute()
+    def multi_int(self, default: List[int] = [], *, name: Optional[str] = None) -> List[int]:
+        multi_int_call = MultiIntCall(name=name, default=default)
+        logger.debug(f"Added MultiIntCall: {name}")
+        return multi_int_call.execute(self.overrides)
 
-    # def bool_input(self, default: bool, *, name: Optional[str] = None) -> bool:
-    #     bool_input_call = BoolInputCall(self, name=name, default=default)
-    #     logger.debug(f"Added BoolInputCall: {name}")
-    #     return bool_input_call.execute()
+    def bool_input(self, default: bool, *, name: Optional[str] = None) -> bool:
+        bool_input_call = BoolInputCall(name=name, default=default)
+        logger.debug(f"Added BoolInputCall: {name}")
+        return bool_input_call.execute(self.overrides)
 
-    # def multi_bool(self, default: List[bool] = [], *, name: Optional[str] = None) -> List[bool]:
-    #     multi_bool_call = MultiBoolCall(self, name=name, default=default)
-    #     logger.debug(f"Added MultiBoolCall: {name}")
-    #     return multi_bool_call.execute()
-
-
-class InvalidSelectionError(Exception):
-    pass
+    def multi_bool(self, default: List[bool] = [], *, name: Optional[str] = None) -> List[bool]:
+        multi_bool_call = MultiBoolCall(name=name, default=default)
+        logger.debug(f"Added MultiBoolCall: {name}")
+        return multi_bool_call.execute(self.overrides)
