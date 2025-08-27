afe4968011fac8fd697705ee573ecc280758c22d
gilad-rubin <gilad.rubin@gmail.com>
2025-08-27 13:55:00 +0300

    removed old version of hypster

--- src/old/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib.metadata import version
-
-__version__ = version(__name__)
-from .config import config
-from .core import Hypster, load, save
-from .hp import HP
-
-__all__ = ["config", "save", "load", "HP"]
